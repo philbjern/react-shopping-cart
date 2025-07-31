bf5ce4edd6df29cb13b30c65c14857fdaf6a066c
philbjern <filipbiernat@gmail.com>
2025-07-31 13:04:16 +0200

    feat: update Shop component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import './Shop.css'
 const Shop = () => {
+
+  const API_URL = `https://fakestoreapi.com`
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  const fetchData = async (url) => {
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      throw new Error('Fetching data from API failed');
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await fetchData(API_URL + '/products');
+        console.log(data);
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    })();
+  }, [])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error}</p>
+
   return (
     <div>
       <h2>Shop</h2>
+      <div className="cards-container">
+        {data && data.map(product => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
